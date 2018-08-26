01cb7cc6f248999607d734b8814d07194bc16352
yangsu1995 <42539152+yangsu1995@users.noreply.github.com>
2018-08-27 00:51:55 +0800

    Add files via upload

---
+++ baike_spider/html_outputer.py
@@ -0,0 +1,30 @@
+#coding:utf8
+class HtmlOutPuter(object):
+    def __init__(self):
+        self.datas = []
+
+    def collect_data(self, data):
+        if data is None:
+            return
+        self.datas.append(data)
+
+
+    def output_html(self):
+        font=open('output.html', 'w' ,encoding="utf-8")
+
+        font.write("<html>")
+        font.write("<head><meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\"></head>")
+        font.write("<body>")
+        font.write("<table>")
+
+        for data in self.datas:
+            font.write("<tr>")
+            font.write("<td>%s</td>" % data['url'].encode('utf-8'))
+            font.write("<td>%s</td>" % data['title'].encode('utf-8'))
+            font.write("<td>%s</td>" % data['summary'].encode('utf-8'))
+            font.write("</tr>")
+
+        font.write("</table>")
+        font.write("</body>")
+        font.write("</html>")
+        font.close()
